e262cb3cc81956226b2c899832347113997cfc11
sunday <llaesasunday@gmail.com>
2024-10-03 01:03:41 +0200

    List.h añadido de nuevo

---
+++ List.h
@@ -0,0 +1,16 @@
+#ifndef LIST_H
+#define LIST_H
+
+template <typename T>
+class List {
+	public:
+		virtual void insert(int pos, T e);
+		virtual void append( T e);
+		virtual void prepend(T e);
+		virtual T remove(int pos);
+		virtual T get(int pos);
+		virtual T get(int pos);
+		virtual T get(int pos);
+		virtual T get(int pos);
+}
+#endif LIST_H
