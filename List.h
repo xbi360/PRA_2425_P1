a71d9eb5acb32dd4d2f5c7a156997a362cbf61f3
sunday <llaesasunday@gmail.com>
2024-10-03 02:15:05 +0200

    he modificado List.h

--- List.h
+++ List.h
@@ -7,10 +7,12 @@ class List {
 		virtual void insert(int pos, T e);
 		virtual void append( T e);
 		virtual void prepend(T e);
-		virtual T remove(int pos);
+		virtual T remove(int pos); 
 		virtual T get(int pos);
-		virtual T get(int pos);
-		virtual T get(int pos);
-		virtual T get(int pos);
-}
+		virtual int search(T e);
+		virtual bool empty();
+		virtual int size();
+		virtual~List();
+};
+
 #endif LIST_H
