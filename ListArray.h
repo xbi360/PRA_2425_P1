f4d87aa57686a614e31f7b8811c3a310818dafd5
sunday <llaesasunday@gmail.com>
2024-10-03 02:15:46 +0200

    he añadido ListArray.h

---
+++ ListArray.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "List.h"
+
+template <typename T>
+class ListArray: public List <T>{
+	private: 
+		T *arr;
+		int max;
+		int n;
+		static const int MINSIZE;
+		void resize(int new_size);
+	public:
+		
+		void insert(int pos, T e) override;
+		ListArray();
+                void append( T e) override;
+                void prepend(T e) override;
+                T remove(int pos) override;
+                T get(int pos) override;
+		int search(T e) override;
+                bool empty() override;
+                int size() override;
+                	
+		T operator [] (int pos);
+		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
+		~ListArray();
+};
